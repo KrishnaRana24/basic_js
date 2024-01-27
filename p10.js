9771ca019f6c39fb9b2d3f0dab24d08e73a16371
KrishnaRana24 <krishname0723@gmail.com>
2024-01-27 15:51:17 +0530

    modify p10

--- p10.js
+++ p10.js
@@ -335,15 +335,15 @@ console.log(rotation([2, 3, 4, 1, 6, 10]));
 // using the following algorithm. Replace every letter in the string with the letter following it in the
 // alphabet (ie. c becomes d, z becomes a). Then capitalize every vowel in this new string (a, e, i,
 // o, u) and finally return this modified string.
-function letter(str) {
-  (str = str.replace(/[a-zA-Z]/g)),
-    function (ch) {
-      if (ch === "z") return "a";
-      if (ch === "Z") return "A";
-      return String.fromCharCode(ch.charCodeAt(0) + 1);
-    };
+
+function LetterChanges(str) {
+  str = str.replace(/[a-zA-Z]/g, function (ch) {
+    if (ch === "z") return "a";
+    if (ch === "Z") return "A";
+    return String.fromCharCode(ch.charCodeAt(0) + 1);
+  });
   return str.replace(/[aeiou]/g, function (ch) {
-    return ch.toUppercase();
+    return ch.toUpperCase();
   });
 }
-console.log(letter("hello&*3"));
+console.log(LetterChanges("Krishna%$#"));
